4e3e0e6e3c4b04657105910f7de3893a7b72f0ec
F4s0lix <janwawrzyniakszkola@gmail.com>
2025-03-13 10:26:24 +0100

    templatka strony i początki backendu

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="pl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>TYTUŁ</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <header>
+        REJESTR WYPOSAŻENIA
+    </header>
+    <nav>
+        <div><a href="app/dodaj.php">dodaj</a></div>
+        <div><a href="app/edytuj.php">edytuj</a></div>
+        <div><a href="app/wypozycz.php">wypożycz</a></div>
+        <div><a href="app/wyszukaj.php">wyszukaj</a></div>
+        <div><a href="app/osoba.php">osoby</a></div>
+    </nav>
+    <main>
+        <div>
+            <h3>ostatnio dodane</h3>
+        </div>
+        <div>
+            <h3>ostatnio wypożyczone</h3>
+        </div>
+    </main>
+    <footer>
+        Stworzono przez: <a href="mailto:jan.wawrzyniak@zhp.pl"> Jan Wawrzyniak</a>
+    </footer>
+</body>
+</html>
---
+++ operations/database.php
@@ -0,0 +1,44 @@
+<?php
+class baza_operacje
+{
+    #klasa pozwala na operacje na bazie danych
+    private $servername = 'localhost';
+    private $username = 'root';
+    private $password = '';
+    private $dbname = 'rejestr_wyposazenia';
+    
+    private $db;
+    
+    private function otworz_polaczenie()
+    {
+        $this->db = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
+    }
+
+    public function ostatnie_dodania()
+    {
+        #funkcja zwraca listę z listami
+        $this->otworz_polaczenie();
+        $query = 'SELECT id, nazwa, ilosc, faktura, miejsce, stan, srodek_trwaly FROM rzecz';
+        $result = $this->db->query($query);
+        $ostatnie = [];
+        if($result->num_rows > 0)
+        {
+            while ($row = $result->fetch_assoc())
+            {
+                $data = [];
+                $data['id'] = $row['id'];
+                $data['nazwa'] = $row['nazwa'];
+                $data['ilosc'] = $row['ilosc'];
+                $data['faktura'] = $row['faktura'];
+                $data['miejsce'] = $row['miejsce'];
+                $data['stan'] = $row['stan'];
+                $data['srodek_trwaly'] = $row['srodek_trwaly'];
+                $ostatnie[] = $data;
+            }
+        }
+        var_dump($ostatnie);
+    }
+}
+$test = new baza_operacje;
+$test->ostatnie_dodania();
+?>
